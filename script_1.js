12a2e22bc7b58a15076d24eebda39efea7459f1f
Laen Lamperouge <157990892+LaenLamperouge@users.noreply.github.com>
2024-02-14 18:08:34 +0200

    add homework 3

---
+++ script_1.js
@@ -0,0 +1,10 @@
+"use strict"
+
+var i = '';
+do {
+    i += '*'; 
+    console.log(i);
+} while (i < '*****');
+
+for (var i = ''; i <= '*****'; i+= '*') {
+    console.log(i);};
---
+++ script_2.js
@@ -0,0 +1,11 @@
+"use strict"
+
+function pow(x, y) {
+        let result = 1;
+    for (let i = 0; i < y; i++) {
+     result *= x;
+    }
+    return result;
+}
+
+console.log(pow(5, 3)); 
